9f9af605d0d517f59f952c9738bf04e4161e8dc5
Goutham AGV <55509228+gouthamagv@users.noreply.github.com>
2020-12-26 16:21:48 +0530

    Update Verilog.c

--- Verilog.c
+++ Verilog.c
@@ -2,12 +2,11 @@
 #include <math.h>
 
 const double PI = 3.1415926536; 
-
   
-void bitReverse(float A[], int N) 
+void bitReverse(double A[], int N) 
 { 
     int i=1;
-    float temp;
+    double temp;
     for(int n=1;n<N;n++)
     {
 		if(n>i)
@@ -26,63 +25,9 @@ void bitReverse(float A[], int N)
     } 
 } 
 
-/*void fft( int A,int B, int N) 
-{ 
-    bitReverse()
-  
-    // j is iota 
-    const complex<double> J(0, 1); 
-    for (int s = 1; s <= log2n; ++s) { 
-        int m = 1 << s; // 2 power s 
-        int m2 = m >> 1; // m2 = m/2 -1 
-        cd w(1, 0); 
-  
-        // principle root of nth complex  
-        // root of unity. 
-        cd wm = exp(J * (PI / m2));  
-        for (int j = 0; j < m2; ++j) { 
-            for (int k = j; k < n; k += m) { 
-  
-                // t = twiddle factor 
-                cd t = w * A[k + m2];  
-                cd u = A[k]; 
-  
-                // similar calculating y[k] 
-                A[k] = u + t;  
-  
-                // similar calculating y[k+n/2] 
-                A[k + m2] = u - t;  
-            } 
-            w *= wm; 
-        } 
-    } 
-} */
-  
-  
-void fft(float A[],int N)
+void fft(double real[],double imaginary[],int N)
 {
-	int M=1,Istep,j;
-	while(N>M)
-	{
-		Istep = M<<1;
-		for(int m=0;m<M;m++)
-		{
-			for(int i=m;i<N;i=i+Istep)
-			{
-				j=i+M;
-				float re = cos(PI *2*m/N)*A[j];
-				float im = sin(PI *2*(m)/N)*A[j];
-				A[j]=A[i]-re;
-				A[N-1+j]=A[N-1+i]-im;
-				A[i]=A[i]+re;
-				A[i+N-1]=A[i+N-1]+im;
-			}
-		}
-		M=Istep;
-	}
-}
-void ffta(double real[],double imaginary[],int N)
-{
+	bitReverse(real,N);
     double Wmk_re,Wmk_im;
     double u_re,u_im;
     double t_re,t_im;
@@ -121,23 +66,21 @@ void ffta(double real[],double imaginary[],int N)
 
 int main() 
 { 
-	const int N=8;
+	const int N=8;             //N should be power of 2
 	float A[N];
 	for(int i=0;i<N;i++)
 	{
-		A[i]=i;
+		scanf("%f",&A[i]); 
 	}
-	bitReverse(A,N);
 	double real[8];
 	double imaginary[8];
-	
 	for(int i=0;i<N;i++)
 	{
 		real[i]=A[i];
 		imaginary[i]=0;
 	}	
 
-	ffta(real,imaginary,8);
+	fft(real,imaginary,8);
 	for(int i=0;i<N;i++)
 	{
 		printf("%lf ,%lf \n",real[i],imaginary[i]);
